42c99896c7458a32cff5ec18a9c15e8164d2b07e
Diego Silvera de Souza <ctk.591@gmail.com>
2019-10-07 00:25:03 -0300

    Se agrega test para validar la adicion de lineas al mapa

--- src/test/MapaTest.java
+++ src/test/MapaTest.java
@@ -9,17 +9,24 @@ import main.interfaz.controles.Mapa;
 public class MapaTest {
 
 	@Test(expected = IllegalArgumentException.class)
-	public void MapaNuevoSinCoordenada() {
+	public void MapaNuevoSinCoordenadaTest() {
 		Mapa mapa = new Mapa(null, 0);
 	}
 
 	@Test(expected = IllegalArgumentException.class)
-	public void CoordenadaConLatitudInvalida() {
+	public void CoordenadaConLatitudInvalidaTest() {
 		Mapa.generarCoordenada(10000, 80);
 	}
 	
 	@Test(expected = IllegalArgumentException.class)
-	public void CoordenadaConLongitudInvalida() {
+	public void CoordenadaConLongitudInvalidaTest() {
 		Mapa.generarCoordenada(-54.2, 10000);
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void AgregarLineaSinCoordenadaTest() {
+		Mapa mapa = new Mapa(Mapa.generarCoordenada(90, 90), 10);
+		
+		mapa.agregarLinea(null, null);
 	}
 }

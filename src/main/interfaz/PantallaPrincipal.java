344d9e755301b6fad00c9fac63a4f51716fedbcd
Diego Silvera de Souza <ctk.591@gmail.com>
2019-10-15 18:22:21 -0300

    Se remueven imports innecesarios

--- src/main/interfaz/PantallaPrincipal.java
+++ src/main/interfaz/PantallaPrincipal.java
@@ -2,15 +2,12 @@ package main.interfaz;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
 
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
-import javax.swing.SwingUtilities;
 import javax.swing.SwingWorker;
 
 import org.openstreetmap.gui.jmapviewer.Coordinate;
